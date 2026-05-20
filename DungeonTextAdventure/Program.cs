4dde334cf2355037ee8a0f75da4b187fabc1f1e5
Ugurlu, Deniz <Deniz.Ugurlu@training.lug-ag.de>
2026-05-20 09:41:38 +0200

    Projektstruktur erstellt

---
+++ DungeonTextAdventure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DungeonTextAdventure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
