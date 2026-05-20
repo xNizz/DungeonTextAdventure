6023e33fc66fff59c872f157796573e21a8671c7
Ugurlu, Deniz <Deniz.Ugurlu@training.lug-ag.de>
2026-05-20 09:58:31 +0200

    Game Startmethode hinzugefügt

--- DungeonTextAdventure/Game.cs
+++ DungeonTextAdventure/Game.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DungeonTextAdventure
+﻿namespace DungeonTextAdventure
 {
     internal class Game
     {
+        public void StartGame()
+        {
+            Console.Clear();
+
+            Console.WriteLine("==================================");
+            Console.WriteLine("      Dungeon Text Adventure      ");
+            Console.WriteLine("==================================");
+            Console.WriteLine();
+
+            Console.WriteLine("Willkommen im Dungeon!");
+            Console.WriteLine("Das Spiel wurde erfolgreich gestartet.");
+            Console.WriteLine();
+
+            Console.WriteLine("Druecke eine Taste, um das Spiel zu beenden...");
+            Console.ReadKey();
+        }
     }
 }
--- DungeonTextAdventure/Program.cs
+++ DungeonTextAdventure/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Game game = new Game();
+
+            game.StartGame();
         }
     }
 }
